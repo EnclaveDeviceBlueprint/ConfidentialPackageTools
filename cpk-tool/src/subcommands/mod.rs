8463f86bffe0d28dadb5e9b48e598cc263a3806b
Paul Howard <paul.howard@arm.com>
2022-06-16 13:44:25 +0100

    Add new pub-key command (included with the sync feature), which just publishes the device public key to the console so that scripts can export it easily to other locations.
    Signed-off-by: Paul Howard paul.howard@arm.com

--- cpk-tool/src/subcommands/mod.rs
+++ cpk-tool/src/subcommands/mod.rs
@@ -14,6 +14,9 @@ mod install;
 #[cfg(feature = "sync")]
 mod sync;
 
+#[cfg(feature = "sync")]
+mod pubkey;
+
 use crate::error::Result;
 
 #[cfg(feature = "build")]
@@ -24,6 +27,9 @@ use crate::subcommands::install::Install;
 
 #[cfg(feature = "sync")]
 use crate::subcommands::sync::Sync;
+
+#[cfg(feature = "sync")]
+use crate::subcommands::pubkey::PubKey;
 
 use structopt::StructOpt;
 
@@ -42,6 +48,12 @@ pub enum Subcommand {
     /// Synchronizes an application class key into the Confidential Package Manager
     #[cfg(feature = "sync")]
     Sync(Sync),
+
+    /// Obtains the public part of the device key pair from the Confidential Package Manager on
+    /// the local host, and writes it to the console as a base64-encoded SubjectPublicKeyInfo
+    /// structure, which is compatible with cloud-based key synchronization mechanisms.
+    #[cfg(feature = "sync")]
+    PubKey(PubKey),
 }
 
 impl Subcommand {
@@ -54,8 +66,10 @@ impl Subcommand {
             Subcommand::Install(cmd) => cmd.run(),
             #[cfg(feature = "sync")]
             Subcommand::Sync(cmd) => cmd.run(),
+            #[cfg(feature = "sync")]
+            Subcommand::PubKey(cmd) => cmd.run(),
             #[cfg(not(any(feature = "build", feature = "install", feature = "sync")))]
-            _ => Ok(())
+            _ => Ok(()),
         }
     }
 }
